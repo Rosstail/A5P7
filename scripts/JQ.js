5b91ac045b545f9be69cd3643831c4454f5814a8
BuildTools <unconfigured@null.spigotmc.org>
2020-03-25 11:40:15 +0100

    fixed myFunction

--- scripts/JQ.js
+++ scripts/JQ.js
@@ -1,28 +1,5 @@
 $(function() {
 
-  
-  $("#anthoPoint").click(function(){
-    percent = 0.17;
-    add_width = (percent*$('.bubbles').parent().width())+'px';
-    if ($("#anthoBubble").width() == 0) {
-      $('#anthoBubble').animate({'width': '+='+add_width}, 500);
-    }
-  })
-  $("#mathPoint").click(function(){
-    percent = 0.17;
-    add_width = (percent*$('.bubbles').parent().width())+'px';
-    if ($("#mathBubble").width() == 0) {
-      $('#mathBubble').animate({'width': '+='+add_width}, 500);
-    }
-  })
-  $("#etiPoint").click(function(){
-    percent = 0.17;
-    add_width = (percent*$('.bubbles').parent().width())+'px';
-    if ($("#etiBubble").width() == 0) {
-      $('#etiBubble').animate({'width': '+='+add_width}, 500);
-    }
-  })
-
   isVisible = false;
   isVisible2 = false;
   isVisible3 = false;
@@ -37,11 +14,15 @@ $(function() {
         //On calcule la distance du premier élément
         $element  = $('#anthoPoint');
         distance = calculateDistance($element, mX, mY);
-        if (distance < 100 && isVisible == false) {
-          $("#anthoPoint").fadeTo("fast", 1);
-          isVisible = true;
+        if (distance < 150 && isVisible == false) {
+          percent = 0.17;
+          add_width = (percent*$('.bubbles').parent().width())+'px';
+          if ($("#anthoBubble").width() == 0) {
+            $('#anthoBubble').animate({'width': '+='+add_width}, 500);
+            isVisible = true;
+          }
         }
-        else if (distance >= 100 && isVisible == true) {
+        else if (distance >= 150 && isVisible == true) {
           isVisible = false;
           $("#anthoPoint").fadeTo("fast", 0.1);
           $(".bubbles").stop().animate({width: 0}, 300 );
@@ -49,11 +30,15 @@ $(function() {
         //On calcule la distance du second élément
         $element2  = $('#mathPoint');
         distance2 = calculateDistance($element2, mX, mY);
-        if (distance2 < 100 && isVisible2 == false) {
-          $("#mathPoint").fadeTo("fast", 1);
-          isVisible2 = true;
+        if (distance2 < 150 && isVisible2 == false) {
+          percent = 0.17;
+          add_width = (percent*$('.bubbles').parent().width())+'px';
+          if ($("#mathBubble").width() == 0) {
+            $('#mathBubble').animate({'width': '+='+add_width}, 500);
+            isVisible2 = true;
+          }
         }
-        else if (distance2 >= 100 && isVisible2 == true) {
+        else if (distance2 >= 150 && isVisible2 == true) {
           isVisible2 = false;
           $("#mathPoint").fadeTo("fast", 0.1);
           $(".bubbles").stop().animate({width: 0}, 300 );
@@ -61,11 +46,15 @@ $(function() {
         //On calcule la distance du troisième élément
         $element3  = $('#etiPoint');
         distance3 = calculateDistance($element3, mX, mY);
-        if (distance3 < 100 && isVisible3 == false) {
-          $("#etiPoint").fadeTo("fast", 1);
-          isVisible3 = true;
+        if (distance3 < 150 && isVisible3 == false) {
+          percent = 0.17;
+          add_width = (percent*$('.bubbles').parent().width())+'px';
+          if ($("#etiBubble").width() == 0) {
+            $('#etiBubble').animate({'width': '+='+add_width}, 500);
+            isVisible3 = true;
+          }
         }
-        else if (distance3 >= 100 && isVisible3 == true) {
+        else if (distance3 >= 150 && isVisible3 == true) {
           isVisible3 = false;
           $("#etiPoint").fadeTo("fast", 0.1);
           $(".bubbles").stop().animate({width: 0}, 300 );
